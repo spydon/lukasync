a3c02aaaa206c21f9ddc5e3e55b55a1305cdf3d7
spydon <lukas.klingsbo@gmail.com>
2013-10-25 09:06:06 +1000

    Added functionality for posting arbritary json to Zurmo

--- src/lukasync/Rest.java
+++ src/lukasync/Rest.java
@@ -1,30 +1,28 @@
 package lukasync;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.Header;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Rest {
-	
+
 	public static JSONObject jsonPost(String urlStr, HashMap<String, String> headers) {
 		return jsonPost(urlStr, headers, null);
 	}
-	
+
 	public static JSONObject jsonPost(String urlStr, HashMap<String, String> headers, JSONObject payload) {
 		try {
 			URI uri = new URI(urlStr);
@@ -33,22 +31,15 @@ public class Rest {
 				post.addHeader("Content-Type", "application/json");
 			if(!headers.containsKey("Accept"))
 				post.addHeader("Accept", "application/json");
-			
+
 			for(String key : headers.keySet())
-				if(!key.equalsIgnoreCase("accept") || !key.equalsIgnoreCase("content-type")) 
+				if(!key.equalsIgnoreCase("accept") || !key.equalsIgnoreCase("content-type"))
 					post.addHeader(key, headers.get(key));
 			if(payload != null) {
-				System.out.println("I HAZ PAYLOAD! " + payload.toString());
-				StringEntity entity = new StringEntity(payload.toString());
+				System.out.println(JSONUtil.jsonToURLEncoding(payload));
+				System.out.println(URLEncoder.encode(JSONUtil.jsonToURLEncoding(payload), "ISO-8859-1"));
+				StringEntity entity = new StringEntity(URLEncoder.encode(JSONUtil.jsonToURLEncoding(payload), "ISO-8859-1"));
 				post.setEntity(entity);
-//				StringWriter writer = new StringWriter();
-//				try {
-//					IOUtils.copy(post.getEntity().getContent(), writer, "UTF-8");
-//				} catch (IllegalStateException | IOException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//				System.out.println(writer.toString());
 			}
 			HttpClient client = HttpClientBuilder.create().build();
 			HttpResponse response = client.execute(post);
@@ -70,10 +61,10 @@ public class Rest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	private static void debugResponse(HttpResponse response) {
 		System.out.println("\nDEBUG: ");
 		System.out.println(response.getStatusLine().getStatusCode() + " " + response.getStatusLine().getReasonPhrase());
